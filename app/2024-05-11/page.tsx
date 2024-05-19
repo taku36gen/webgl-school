828ebd15f1e69c2706ed6ac3f61d64efd494b356
Takumi-K22 <takumi.kawana03@gmail.com>
2024-05-19 17:01:57 +0900

    modified homework/page.tsx

--- app/2024-05-11/page.tsx
+++ app/2024-05-11/page.tsx
@@ -6,15 +6,20 @@ import { useEffect } from "react";
 export default function Page() {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      window.addEventListener(
-        "DOMContentLoaded",
-        () => {
-          const wrapper = document.querySelector("#webgl");
-          const app = new ThreeApp(wrapper);
-          app.render();
-        },
-        false
-      );
+      //  useEffect内は既にDOMのロードが完了しているため、
+      //  この位置のDOMContentLoadedは呼び出されない
+      //   window.addEventListener(
+      //     // "DOMContentLoaded",
+      //     // () => {
+      const wrapper = document.querySelector("#webgl");
+      if (wrapper) {
+        const app = new ThreeApp(wrapper);
+        app.render();
+      }
+
+      // },
+      //     false
+      //   );
     }
   }, []);
   return <div id="webgl"></div>;
