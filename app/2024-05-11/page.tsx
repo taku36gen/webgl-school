3320d2554c68cd58ba235823954936aa3e607081
Takumi-K22 <takumi.kawana03@gmail.com>
2024-05-19 15:19:50 +0900

    modified client process

--- app/2024-05-11/page.tsx
+++ app/2024-05-11/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import ThreeApp from "./ThreeApp";
+import { useEffect } from "react";
 
-export default function page() {
-  window.addEventListener(
-    "DOMContentLoaded",
-    () => {
-      const wrapper = document.querySelector("#webgl");
-      const app = new ThreeApp(wrapper);
-      app.render();
-    },
-    false
-  );
-  return <div id="webgl">page</div>;
+export default function Page() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener(
+        "DOMContentLoaded",
+        () => {
+          const wrapper = document.querySelector("#webgl");
+          const app = new ThreeApp(wrapper);
+          app.render();
+        },
+        false
+      );
+    }
+  }, []);
+  return <div id="webgl"></div>;
 }
