0307254a4c6dd33a3685c4317d3619354ca2ba60
Takumi-K22 <takumi.kawana03@gmail.com>
2024-05-19 15:03:02 +0900

    add three

--- app/2024-05-11/page.tsx
+++ app/2024-05-11/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import ThreeApp from "./ThreeApp";
+
 export default function page() {
+  window.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      const wrapper = document.querySelector("#webgl");
+      const app = new ThreeApp(wrapper);
+      app.render();
+    },
+    false
+  );
   return <div>page</div>;
 }
